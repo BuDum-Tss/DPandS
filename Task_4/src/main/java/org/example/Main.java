0b83daa3f42309836be2b188c2f99322a910a0fd
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2023-09-08 17:46:51 +0700

    solution added

--- Task_4/src/main/java/org/example/Main.java
+++ Task_4/src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Thread thread = new Thread(()->{
+      while (!Thread.interrupted()){
+        System.out.println("This is text.");
+      }
+    });
+    thread.start();
+    try {
+      Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+    } catch (InterruptedException e) {
+      System.out.println("Thread has been interrupted");
+    }
+    thread.interrupt();
+    System.out.println("Thread is interrupted");
   }
 }
