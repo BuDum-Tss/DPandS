8858002bbd2a652e5de014606e25883c984404cf
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2023-09-13 21:45:40 +0700

    solution added

--- Task_6/src/main/java/org/example/Founder.java
+++ Task_6/src/main/java/org/example/Founder.java
@@ -2,16 +2,36 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public final class Founder {
 
   private final List<Runnable> workers;
+  private final CyclicBarrier barrier;
 
   public Founder(final Company company) {
-    this.workers = new ArrayList<>(company.getDepartmentsCount());
+    barrier = new CyclicBarrier(company.getDepartmentsCount(), company::showCollaborativeResult);
+    List<Runnable> list = new ArrayList<>();
+    for (int i = 0; i < company.getDepartmentsCount(); i++) {
+      int finalI = i;
+      list.add(() -> {
+        company.getFreeDepartment(finalI).performCalculations();
+        System.out.println("Department finished! Waiting others...");
+        try {
+          barrier.await();
+        } catch (InterruptedException ex) {
+
+        } catch (BrokenBarrierException ex) {
+          throw new RuntimeException(ex);
+        }
+      });
+    }
+    this.workers = list;
   }
 
   public void start() {
+    Thread thread = Thread.currentThread();
     for (final Runnable worker : workers) {
       new Thread(worker).start();
     }
--- Task_6/src/main/java/org/example/Main.java
+++ Task_6/src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Company company = new Company(4);
+    Founder founder = new Founder(company);
+    founder.start();
   }
 }
