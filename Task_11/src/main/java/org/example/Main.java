f82a21685d4909e1e6a1a54c05202cae816444e7
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2023-10-21 21:44:38 +0700

    solution

--- Task_11/src/main/java/org/example/Main.java
+++ Task_11/src/main/java/org/example/Main.java
@@ -1,8 +1,38 @@
 package org.example;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Semaphore semaphore1 = new Semaphore(1);
+    Semaphore semaphore2 = new Semaphore(1);
+    Runnable task = () -> {
+      for (int i = 0; i < 10; i++) {
+        try {
+          semaphore2.acquire();
+          semaphore1.acquire();
+          semaphore2.release();
+          System.out.println("Task " + i + " in 2nd thread");
+          semaphore1.release();
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+
+      }
+    };
+    Thread thread = new Thread(task);
+    thread.start();
+    for (int i = 0; i < 10; i++) {
+      try {
+        semaphore2.acquire();
+        semaphore1.acquire();
+        semaphore2.release();
+        System.out.println("Task " + i + " in main thread");
+        semaphore1.release();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
