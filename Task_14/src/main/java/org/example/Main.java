24f5714b05bf54bf375b7adb6640d2f2c02d0809
BaDum-Tss <99732685+BuDum-Tss@users.noreply.github.com>
2023-10-24 22:41:03 +0700

    solution

--- Task_14/src/main/java/org/example/Main.java
+++ Task_14/src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Semaphore widget = new Semaphore(0);
+    Semaphore module = new Semaphore(0);
+    Semaphore partA = new Semaphore(0);
+    Semaphore partB = new Semaphore(0);
+    Semaphore partC = new Semaphore(0);
+
+    List<Thread> producers = new ArrayList<>();
+    producers.add(new Thread(new SomeProducer("Widget", widget, new Semaphore[]{widget, module}, 0)));
+    producers.add(new Thread(new SomeProducer("Module", module, new Semaphore[]{partA, partB}, 0)));
+    producers.add(new Thread(new SomeProducer("Part A", partA, new Semaphore[]{}, 1000)));
+    producers.add(new Thread(new SomeProducer("Part B", partB, new Semaphore[]{}, 2000)));
+    producers.add(new Thread(new SomeProducer("Part C", partC, new Semaphore[]{}, 3000)));
+
+    producers.forEach(Thread::start);
   }
 }
